4ff6a1ee14ff843302010f60fbff71c3778eeca8
Matthew Gonzalez <matthewgonzalez7725@gmail.com>
2024-05-28 14:56:41 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Challenge4-Code-Quiz
+
+
+## Description
+
+In this weeks chalenge we were required to create a timed quiz about coding with multiple choice options. The application is ran from the browser and features dynamic HTML and CSS code by using Javascript.
+
+## Usage
+
+The link of the webpage is: 
+
+Step 1:Load the webpage.
+Step 2:Click "start" button to start the quiz and begin the timer.
+Step 3:Choose one of the four multiple choice options and you will see whether your answer is correct or incorrect.
+Step 4:When you finish the quiz, or the time runs out, your score will be displayed and you can submit your initials.
+Step 5:After submiting your initials, you can view your highscore and comepare it to top scores.
+Step 6:Clear the local record by simply hit the "clear Highscores" button.
+
+## Credits
+
+For help on which HTML semantic elements to use as propper replacements I used W3Schools.
+
+https://www.w3schools.com/html/html5_semantic_elements.asp
+
+For help with CSS selectors i also referred to W3Schools.
+
+https://www.w3schools.com/CSS/css_selectors.asp
+
+For help with JavaScript I referenced W3Schools.
+
+https://www.w3schools.com/js/
+
+## License
+
+MIT License
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,16 @@ var choiceListEl=document.getElementById("choice-list")
 var timerEl=document.getElementById("timer")
 var messageEl=document.getElementById("message")
 var finalScoreEl=document.getElementById("score")
+var topScoresEl=document.querySelector("#highscore ol")
+var submitBtn=document.getElementById("submit")
+var goBackBtn=document.getElementById("goback-btn")
+var clearTopScoresBtn=document.getElementById("clearHighscore")
+var topScoresBtn=document.querySelector("header button")
 var setIntervalId
 var timeLeft=questionData.length * 15
 var index=0
+
+var scores=[]
 
 
 function startQuiz(){
@@ -21,7 +28,7 @@ function startQuiz(){
 }
 function renderQuestion(){
     messageEl.innerHTML=""
-    questionTitleEl.textContent=questionData[index].title
+    questionTitleEl.textContent=questionData[index].title;
     choiceListEl.textContent=""
     for(var i=0; i<questionData[index].choices.length; i++){
         var li=document.createElement("li")
@@ -65,5 +72,55 @@ function endQuiz(){
     initialInputEl.classList.remove("hide")
     finalScoreEl.textContent=timerEl.textContent
 }
+
+function storeScores(){
+    var initials=document.getElementById("initial").ariaValueMax.trim();
+    if(initials !== ""){
+        var score=parseInt(finalScoreEl.textContent)
+        var topScores=JSON.parse(localStorage.getItem("topScores")) || []
+        topScores.push({initials:initials,score:score})
+        localStorage.setItem("topScores",JSON.stringify(topScores))
+        displayTopScores()
+        initialInputEl.classList.add("hide")
+        highscoreEl.classList.remove("hide")
+    }else{
+        alert("Enter your initials!")
+    }
+
+
+}
+function displayTopScores(){
+    topScoresEl.innerHTML=""
+    var topScores=JSON.parse(localStorage.getItem("topScores")) || []
+    topScores.sort((a,b)=>b.score-a.score)
+    topScores.forEach(function(score){
+        var li=document.createElement("li")
+        li.textContent=score.initials+"-"+score.score
+        topScoresEl.appendChild(li)
+    })
+}
+
+function clearTopScores(){
+    localStorage.removeItem("topScores")
+    displayTopScores()
+}
+
+
 startQuizEl.addEventListener("click",startQuiz)
 choiceListEl.addEventListener("click",nextQuestion)
+submitBtn.addEventListener("click",storeScores)
+goBackBtn.addEventListener("click",function(){
+    highscoreEl.classList.add("hide")
+    introEl.classList.remove("hide")
+    index=0
+    timeLeft=questionData.length * 15
+    clearInterval(setIntervalId)
+    timerEl.textContent=timeLeft
+})
+clearTopScoresBtn.addEventListener("click",clearTopScores)
+topScoresBtn.addEventListener("click",function(displayTopScores){
+    introEl.classList.add("hide")
+    highscoreEl.classList.remove("hide")
+});
+
+displayTopScores();
